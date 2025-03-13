01bc210861af2780677e89a0311ceb7e4cb0b8aa
GuilhermeFogolin <guilhermefogolin02@gmail.com>
2025-03-12 21:25:59 -0300

    Adicionando o projeto Calculadora de IMC

--- CalculadoraIMC/app/src/main/java/br/com/fecapccp/calculadoraimc/MainActivity.java
+++ CalculadoraIMC/app/src/main/java/br/com/fecapccp/calculadoraimc/MainActivity.java
@@ -1,6 +1,9 @@
 package br.com.fecapccp.calculadoraimc;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,38 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    // Mé-todo para Setar informações:
+
+    public void enviar(View v) {
+
+        // Instanciamento dos objetos do app:
+
+        EditText campoPeso = findViewById(R.id.et_peso);
+        EditText campoAltura = findViewById(R.id.et_altura);
+        TextView resultado = findViewById(R.id.tv_resultado);
+
+        // Cálculos
+
+        double peso = Double.parseDouble(campoPeso.getText().toString());
+        double altura = Double.parseDouble(campoAltura.getText().toString());
+        double calcular = peso / altura;
+
+        // Apresentar o resultado
+
+        resultado.setText("Seu IMC é " + String.format("%.3f", calcular) + "!");
+    }
+
+    // Método para Resetar informações
+
+    public void limpar(View v) {
+        TextView resultado = findViewById(R.id.tv_resultado);
+        EditText campoNome = findViewById(R.id.et_peso);
+        EditText campoIdade = findViewById(R.id.et_altura);
+        resultado.setText("-----");
+        campoNome.setText("");
+        campoIdade.setText("");
+
     }
 }
