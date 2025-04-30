8387e915c55d34afa67b005c8d1dd38bb607a859
GuilhermeFogolin <guilhermefogolin02@gmail.com>
2025-04-30 19:47:58 -0300

    Adicionando métodos do SeekBar

--- SeekBar/app/src/main/java/com/fecap/ccp/seek_bar/MainActivity.java
+++ SeekBar/app/src/main/java/com/fecap/ccp/seek_bar/MainActivity.java
@@ -1,6 +1,8 @@
 package com.fecap.ccp.seek_bar;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SeekBar seekBarEscala;
+    private TextView textResultado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        seekBarEscala = findViewById(R.id.seekBarEscala);
+        textResultado = findViewById(R.id.textResultado);
+
+        seekBarEscala.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textResultado.setText("onProgress");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                textResultado.setText("onStart");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textResultado.setText("onStop");
+            }
         });
+
     }
 }
