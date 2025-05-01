983d8d037edf044a9355b0f42404af3420bb5166
GuilhermeFogolin <guilhermefogolin02@gmail.com>
2025-04-30 21:49:17 -0300

    Adicionando funcionalidade para calcular gorjeta

--- Gorjeta/app/src/main/java/com/fecap/ccp/gorjeta/MainActivity.java
+++ Gorjeta/app/src/main/java/com/fecap/ccp/gorjeta/MainActivity.java
@@ -1,6 +1,9 @@
 package com.fecap.ccp.gorjeta;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,62 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editValor;
+    private TextView text_porcentagem, text_gorjeta, text_total;
+    private SeekBar seekBarGorjeta;
+    private double porcentagem = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        editValor = findViewById(R.id.editValor);
+        text_gorjeta = findViewById(R.id.text_gorjeta);
+        text_porcentagem = findViewById(R.id.text_porcentagem);
+        text_total = findViewById(R.id.text_total);
+        seekBarGorjeta = findViewById(R.id.seekBarGorjeta);
+
+        // Manipulação sem botão: Seekbar - "ouvinte"
+
+        seekBarGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                porcentagem = i;
+                text_porcentagem.setText(Math.round(porcentagem) + "%");
+                calcular();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO: O método calcular pode ser colocado aqui também.
+            }
         });
+
+    }
+
+    public void calcular() {
+        String valorRecuperado = editValor.getText().toString();
+
+        if (valorRecuperado == null || valorRecuperado.equals("")) {
+
+        } else {
+
+            // Converter String para Double
+
+            double valorDigitado = Double.parseDouble(valorRecuperado);
+
+            // Calcular a gorjeta total:
+
+            double gorjeta = valorDigitado * (porcentagem / 100);
+
+            // Exibir a gorjeta
+
+            text_gorjeta.setText("R$" + gorjeta);
+        }
     }
 }
